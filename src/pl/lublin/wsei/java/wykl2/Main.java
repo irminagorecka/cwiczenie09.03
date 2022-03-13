8152ccf09de046990b56df03c570a0cc3bf299b5
irminagorecka <irmina.gorecka@gmail.com>
2022-03-13 19:12:44 +0100

    wczytywanie danych z klawiatury, zmienne, if else

--- src/pl/lublin/wsei/java/wykl2/Main.java
+++ src/pl/lublin/wsei/java/wykl2/Main.java
@@ -1,6 +1,33 @@
 package pl.lublin.wsei.java.wykl2;
+import java.util.Scanner;
+
 public class Main {
      public static void main (String[] args){
-         System.out.println("Hello Java world");
+         //definiowanie zmiennej wczytywanej z klawiatury:
+         Scanner scanner = new Scanner(System.in); //definiowanie scannnera
+         System.out.println("Podaj imię: ");
+         //scanner.nextLine(); //wykorzystanie Scannera - Ctrl+Alt+V, polecenie się zmieni na takie jak poniżej:
+         String name = scanner.nextLine();
+
+         System.out.println("Cześć " + name);
+
+         //if
+         if (!name.isEmpty() && (!name.endsWith("a") || name.equals("Barnaba")|| name.equals("Kuba"))) {
+             System.out.println("Jesteś męzczyzną");
+         } else if (!name.isEmpty() && name.endsWith("a")) {
+             System.out.println("Jesteś kobietą");
+         } else {
+             System.out.println("Imie nie może być puste");
+         }
+         //if (!name.endsWith("a")){
+             //System.out.println("Jesteś męzczyzną");
+        // }
+
+
+
+
+
+
+
          }
 }
