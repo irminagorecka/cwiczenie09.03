79c57f3dce7da2ec8c5e5f4a07b0378a6b96c5e8
irminagorecka <irmina.gorecka@gmail.com>
2022-03-13 01:47:29 +0100

    Pierwsze zasilenie

---
+++ src/pl/lublin/wsei/java/wykl2/Main.java
@@ -0,0 +1,6 @@
+package pl.lublin.wsei.java.wykl2; //java która jest w tym pliku ma stać się cześcią pakietu
+public class Main {
+     public static void main (String[] args){
+         System.out.println("Hello Java world");
+         }
+}
