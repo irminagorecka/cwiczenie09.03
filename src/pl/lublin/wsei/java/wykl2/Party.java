8aba8201c5341bdca785f7553246952760f78574
irminagorecka <irmina.gorecka@gmail.com>
2022-04-12 23:12:48 +0200

    cd12.04

--- src/pl/lublin/wsei/java/wykl2/Party.java
+++ src/pl/lublin/wsei/java/wykl2/Party.java
@@ -5,19 +5,40 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Party  {
-    private List<String>guests=new ArrayList<>(); //private widoczne tylko w obrębie tej klasy tj Party
+    private List<Guest>guests=new ArrayList<>(); //private widoczne tylko w obrębie tej klasy tj Party
     Scanner scanner = new Scanner(System.in);
 
 
     public void addGuest(){
         System.out.println("Podaj imię gościa");
         String name = scanner.nextLine();
-        guests.add(name);
+
+        System.out.println("Podaj preferowany posiłek:");
+        String meal = scanner.nextLine();
+
+        System.out.println("Podaj nr telefonu:");
+        int phoneNumber = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Czy weganin Y/N");
+        String isVeganString = scanner.nextLine();
+
+        boolean isVegan;
+
+if (isVeganString.equals("Y")){isVegan=true;
+}else {
+    isVegan=false;
+}
+        //new Guest(name, meal, phoneNumber, isVegan); // i dokonuję ekstrakcji za pomocą Ctrl Alt V. Powstaje to co poniżej:
+        Guest guest = new Guest(name, meal, phoneNumber, isVegan);
+
+        guests.add(guest); //spodziewamy się obiektu typu Guest}
+
+    /*public void displayGuests() {
+        for (Guest guest : guests) {
+            System.out.println(guest);
+        }/*
     }
 
-    public void displayGuests() {
-        for (String guest : guests) {
-            System.out.println(guest);
-        }
-    }
-}
+    //String isVeganString= isVegan ? "tak" : "nie";
+*/
+}}
